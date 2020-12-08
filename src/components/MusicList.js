30b629e1915c91f4062ade5b0d6b0d60aeb96c9e
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-08 20:36:57 +0000

    project initialised. Music container and initial component added and rendering to browser

---
+++ src/components/MusicList.js
@@ -0,0 +1,8 @@
+const MusicList = () => {
+
+return (
+    <p>top songs go in here</p>
+)
+
+}
+export default MusicList;
---
+++ src/containers/MusicContainer.js
@@ -0,0 +1,13 @@
+import MusicList from "../components/MusicList";
+
+const MusicContainer = () => {
+
+    return (
+        <>
+        <h1>Welcome to the iTunes Top 20 songs in the UK...</h1>
+        <MusicList />
+        </>
+    )
+}
+
+export default MusicContainer;
