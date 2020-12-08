8b2486dd80ebfd638b3d204737920a7a41aa1a33
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-12-08 20:41:20 +0000

    SongDetails component added and rendering to browser through hooks.

---
+++ src/components/SongDetails.js
@@ -0,0 +1,8 @@
+const SongDetails = () => {
+
+    return (
+        <p>Song details go here!</p>
+    )
+}
+
+export default SongDetails;
--- src/containers/MusicContainer.js
+++ src/containers/MusicContainer.js
@@ -1,4 +1,5 @@
 import MusicList from "../components/MusicList";
+import SongDetails from "../components/SongDetails";
 
 const MusicContainer = () => {
 
@@ -6,6 +7,7 @@ const MusicContainer = () => {
         <>
         <h1>Welcome to the iTunes Top 20 songs in the UK...</h1>
         <MusicList />
+        <SongDetails />
         </>
     )
 }
